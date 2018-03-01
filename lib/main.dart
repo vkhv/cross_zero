476518eedd15f427830bcf39e634505098385bd6
Владислав А. Хвостов <khvostov@yamoney.ru>
2018-03-01 23:26:14 +0300

    Add state

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,66 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
+// Uncomment lines 7 and 10 to view the visual layout at runtime.
+//import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
-void main() => runApp(new MyApp());
+void main() {
+  //debugPaintSizeEnabled = true;
+  runApp(new MyApp());
+}
 
+// TapboxA manages its own state.
 
+//------------------------- TapboxA ----------------------------------
+
+class TapboxA extends StatefulWidget {
+  TapboxA({Key key}) : super(key: key);
+
+  @override
+  _TapboxAState createState() => new _TapboxAState();
+}
+
+class _TapboxAState extends State<TapboxA> {
+  bool _active = false;
+
+  void _handleTap() {
+    setState(() {
+      _active = !_active;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return new GestureDetector(
+      onTap: _handleTap,
+      child: new Container(
+        child: new Center(
+          child: new Text(
+            _active ? 'X' : '0',
+            style: new TextStyle(fontSize: 60.0, color: Colors.white),
+          ),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: new BoxDecoration(
+          color: _active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+      ),
+    );
+  }
+}
+
+//------------------------- MyApp ----------------------------------
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    const emptySection = '[]';
     return new MaterialApp(
-      title: 'Cross zero',
+      title: 'Flutter Demo',
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text('Cross zero'),
+          title: new Text('Flutter Demo'),
         ),
         body: new Center(
           child: new GridView.count(
@@ -21,26 +69,19 @@ class MyApp extends StatelessWidget {
             crossAxisSpacing: 10.0,
             crossAxisCount: 3,
             children: <Widget>[
-              getItem('X'),
-              getItem('X'),
-              getItem('X'),
-              getItem('X'),
-              getItem('X'),
-              getItem('X'),
-              getItem('X'),
-              getItem('X'),
-              getItem('X')
+              new TapboxA(),
+              new TapboxA(),
+              new TapboxA(),
+              new TapboxA(),
+              new TapboxA(),
+              new TapboxA(),
+              new TapboxA(),
+              new TapboxA(),
+              new TapboxA()
             ],
-          ),
+          )
         ),
       ),
-    );
-  }
-  Widget getItem(String character) {
-    return new Container( // grey box
-      child: new Center(
-          child: new Text(character, style: new TextStyle(fontSize: 80.0),)),
-      color: Colors.grey[300],
     );
   }
 }
